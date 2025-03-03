8a63d2249e7b95284d25dedd4546beebf193bcaa
Nalla Suman <aliensgroup04@gmail.com>
2025-03-03 14:52:53 +0530

    Update cooking.py

--- cooking.py
+++ cooking.py
@@ -22,7 +22,7 @@ class Recipe(BaseModel):
 output_parser = PydanticOutputParser(pydantic_object=Recipe)
 
 # Load API key
-GOOGLE_API_KEY = os.getenv(""AIzaSyCBhbuJbxjlghoZ3X1HQhS_qwuMpSE1wC0"")
+GOOGLE_API_KEY = os.getenv("AIzaSyCBhbuJbxjlghoZ3X1HQhS_qwuMpSE1wC0")
 model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=GOOGLE_API_KEY)
 
 # Prompt Template
